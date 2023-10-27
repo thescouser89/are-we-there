330e2e747c66e3f1615c71ec5d4c97c9e32d01d3
Dustin Kut Moy Cheung <dcheung@redhat.com>
2023-10-27 16:09:44 -0400

    Initial commit

---
+++ src/main/java/org/jboss/pnc/yet/GitClient.java
@@ -0,0 +1,57 @@
+package org.jboss.pnc.yet;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class GitClient {
+
+    private String repository;
+    private File tempDir;
+    private Git git;
+
+    public GitClient(String repository) throws Exception {
+        this.repository = repository;
+        tempDir = Files.createTempDirectory("yet-git-client").toFile();;
+
+        git = Git.cloneRepository()
+                 .setURI(repository)
+                 .setDirectory(tempDir)
+                 .setCloneAllBranches(true)
+                 .call();
+    }
+
+    public String getParentOfCommit(String commitToFind) throws Exception {
+
+        ObjectId commitId = git.getRepository().resolve(commitToFind);
+        RevWalk revWalk = new RevWalk(git.getRepository());
+        RevCommit commit = revWalk.parseCommit(commitId);
+
+        return commit.getParent(0).getId().getName();
+    }
+
+    public String getCommiterOfCommit(String commitToFind) throws Exception {
+        ObjectId commitId = git.getRepository().resolve(commitToFind);
+        RevWalk revWalk = new RevWalk(git.getRepository());
+        RevCommit commit = revWalk.parseCommit(commitId);
+
+        return commit.getAuthorIdent().getName();
+    }
+
+    public String getMessageOfCommit(String commitToFind) throws Exception {
+        ObjectId commitId = git.getRepository().resolve(commitToFind);
+        RevWalk revWalk = new RevWalk(git.getRepository());
+        RevCommit commit = revWalk.parseCommit(commitId);
+
+        return commit.getShortMessage();
+    }
+
+    public boolean isCommitPresent(String commitToFind) throws Exception {
+        ObjectId commitId = git.getRepository().resolve(commitToFind);
+        return commitId != null;
+    }
+}
